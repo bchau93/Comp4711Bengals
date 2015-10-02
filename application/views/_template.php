25eef8dc0b27b9bd785bf2b81857dd070d1cff18
bchau <bchau93@hotmail.com>
2015-10-02 16:32:18 -0700

    Finish assignment 1

--- application/views/_template.php
+++ application/views/_template.php
@@ -18,7 +18,7 @@ if (!defined('APPPATH'))
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
-        <link rel="stylesheet" type="text/css" href="/assets/css/homepage.css"/>
+        
     </head>
     <body>
         <div class="container">
