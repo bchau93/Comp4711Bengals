b438d90a09e1e727d3054d9c21d141b73868c2a2
bchau <bchau93@hotmail.com>
2015-10-03 16:38:12 -0700

    Added AFC North Standing Title to Conference page

--- application/views/conference.php
+++ application/views/conference.php
@@ -1,11 +1,12 @@
-<div class="row">
-    {teams}
+<div class="row"> 
     <div class="text">
+        <h2>AFC NORTH STANDINGS</h2>
+        {teams}
         <ul>TEAM: <strong>{who}</strong>     WINS: <strong>{wins}</strong>    
             LOSSES: <strong>{losses}</strong>     TIES: <strong>{ties}</strong>   
             HOME: <strong>{home}</strong>    ROAD: <strong>{road}</strong>    
             STREAK: <strong>{streak}</strong></ul>
-    </div>
-    {/teams}
+        {/teams}
+    </div>  
 </div>
 
